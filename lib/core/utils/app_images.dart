438ee386629cbd37387b847d4d2f4277e593cccf
Jiko <youstinahabib422@gmail.com>
2025-09-15 01:18:13 +0300

    Updated pubspec.yaml to include flutter_svg dependency and organized asset paths.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/app_icon.png
+  static const String assetsImagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for assetsImagesSplash1
+  /// assets/images/splash1.svg
+  static const String assetsImagesSplash1 = "assets/images/splash1.svg";
+
+  /// Assets for assetsImagesSplash2
+  /// assets/images/splash2.svg
+  static const String assetsImagesSplash2 = "assets/images/splash2.svg";
+
+  /// Assets for assetsImagesSplash3
+  /// assets/images/splash3.svg
+  static const String assetsImagesSplash3 = "assets/images/splash3.svg";
+}
+
