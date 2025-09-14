654d8116eb2bdb55d8f5aef509df6a5d510c1cc9
Jiko <youstinahabib422@gmail.com>
2025-09-15 02:21:27 +0300

    splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -6,8 +8,17 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.assetsImagesSplash1),
+          ],
+        ),
+        SvgPicture.asset(Assets.assetsImagesSplash2),
+        SvgPicture.asset(Assets.assetsImagesSplash3, fit: BoxFit.fill),
       ],
     );
   }
