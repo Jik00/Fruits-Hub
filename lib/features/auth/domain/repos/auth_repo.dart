1c975a1ead1a2094a83c5c456416c7ecf8e7a4b3
Jiko <youstinahabib422@gmail.com>
2025-10-26 23:14:19 +0300

    Either return type in authRepo

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failures.dart';
 import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
       String email, String password, String username);
 }
