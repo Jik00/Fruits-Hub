86980a2214b8fe351173f5ec16a9d470e172df2a
Jiko <youstinahabib422@gmail.com>
2025-12-06 16:14:28 +0200

    adding customBlocObserver + provide it in main

--- lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
@@ -5,6 +5,8 @@ import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubit/signup
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
+import '../../../../../core/helper_functions/build_error_bar.dart';
+
 class SignUpViewBodyBlocConsumer extends StatelessWidget {
   const SignUpViewBodyBlocConsumer({
     super.key,
@@ -12,21 +14,18 @@ class SignUpViewBodyBlocConsumer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer< SignupCubit, SignupState>(
+    return BlocConsumer<SignupCubit, SignupState>(
       listener: (context, state) {
-       if (state is SignupSuccess) {
-       }
-       if (state is SignupFailure) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(state.message)),
-        );
-       }
+        if (state is SignupSuccess) {}
+        if (state is SignupFailure) {
+          buildErrorBar(context, state.message);
+        }
       },
       builder: (context, state) {
         return ModalProgressHUD(
-          progressIndicator: const CustomLoadingBar(),
-          inAsyncCall: state is SignupLoading? true : false,
-          child: const SignupViewBody());
+            progressIndicator: const CustomLoadingBar(),
+            inAsyncCall: state is SignupLoading ? true : false,
+            child: const SignupViewBody());
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruits_hub/core/helper_functions/ongenerate_routes.dart';
+import 'package:fruits_hub/core/services/custom_bloc_observer.dart';
 import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
@@ -18,6 +20,7 @@ void main() async {
   );
   await Prefs.init();
   setupGetIt();
+  Bloc.observer = CustomBlocObserver();
 
   runApp(const MyApp());
 }
