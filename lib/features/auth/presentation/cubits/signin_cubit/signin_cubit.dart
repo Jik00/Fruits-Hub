d4bdc4aa808001f36fde8daf43e24e398f4b252d
Jiko <youstinahabib422@gmail.com>
2025-12-06 23:56:47 +0200

    login with mail & pass - 4 - create signin cubit

---
+++ lib/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'signin_state.dart';
+
+class SignInCubit extends Cubit<SignInState> {
+  SignInCubit(this.authRepo) : super(SignInInitial());
+
+  final AuthRepo authRepo;
+
+  Future<void> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    emit(SignInLoading as SignInState);
+
+    final result = await authRepo.signInWithEmailAndPassword(email, password);
+
+    result.fold(
+      (failure) => emit(SignInFailure(message: failure.message)),
+      (user) => emit(SignInSuccess(user: user)),
+    );
+  }
+}
